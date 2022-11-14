ebce9e7e00eb7acd3aaf329f24332301270b7949
_Burnside <33724737+Octopus136@users.noreply.github.com>
2022-11-14 14:38:45 +0800

    Add files

---
+++ creep.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<cstdlib>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif // _WIN32
+
+void sleepcp(int milliseconds)
+{
+    #ifdef _WIN32
+        Sleep(milliseconds);
+    #else
+        usleep(milliseconds * 1000); 
+    #endif // _WIN32
+}
+
+void creep(int ground_length, std::string name, int delay_ms){
+    int dir = 1;
+    int pos = 0;
+    std::reverse(name.begin(), name.end());
+    while(true){
+        system("clear");
+        for(int i = 0; i < pos; i++){
+            std::cout << " ";
+        }
+        std::cout << name << std::endl;
+        if(pos == ground_length - name.length()){
+            dir = -1;
+            std::reverse(name.begin(), name.end());
+        }
+        if(pos == 0){
+            dir = 1;
+            std::reverse(name.begin(), name.end());
+        }
+        for(int i = 0; i < ground_length; i++){
+            std::cout << "-";
+        }
+        std::cout << std::endl;
+        sleepcp(delay_ms);
+        pos += dir;
+    }
+}
+int main(int argc, char* argv[]){
+    try {
+        if(argc != 2 && argc != 0) {
+            throw "Invalid number of arguments";
+        }
+    }
+    catch(const char* msg) {
+        std::cerr << msg << std::endl;
+        return 1;
+    }
+    int ground_length, delay_ms;
+    if(argc == 2) {
+        std::string ground_length_str = argv[0];
+        std::string delay_ms_str = argv[1];
+        try {
+            for(int i = 0; i < ground_length_str.length(); i++){
+                if(!isdigit(ground_length_str[i])){
+                    throw "Invalid ground length";
+                }
+            }
+            for(int i = 0; i < delay_ms_str.length(); i++){
+                if(!isdigit(delay_ms_str[i])){
+                    throw "Invalid delay";
+                }
+            }
+        }
+        catch(const char* msg) {
+            std::cerr << msg << std::endl;
+            return 1;
+        }
+        ground_length = std::stoi(ground_length_str);
+        delay_ms = std::stoi(delay_ms_str);
+    }
+    else {
+        ground_length = 20;
+        delay_ms = 100;
+    }
+    std::string name;
+    std::cout << "Who will creep : ";
+    std::cin >> name;
+    freopen("params", "r", stdin);
+    try {
+        if(ground_length <= name.length()){
+            throw "Ground length is too small!";
+        }
+    }
+    catch (const char* msg) {
+        std::cerr << msg << std::endl;
+        return 1;
+    } 
+    creep(ground_length, name, delay_ms);
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+g++ creep.cpp -o creep
+./creep
